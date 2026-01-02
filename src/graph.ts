5c070ca40858111a9204485afb779e9a1977296b
rakshith_s <rakshithsantosh@gmail.com>
2026-01-02 20:01:05 +0530

    creating routers

--- src/graph.ts
+++ src/graph.ts
@@ -38,19 +38,62 @@ async function frontDeskSupport(state: typeof StateAnnotation.State) {
 }
 
 function marketingSupport(state: typeof StateAnnotation.State) {
-  //logic for frontdesk support
-
+  //logic for marketing support
+  console.log("In marketing support");
   return state;
 }
 
 function LearningSupport(state: typeof StateAnnotation.State) {
-  //logic for frontdesk support
-
+  //logic for learning support
+  console;
   return state;
+}
+
+function whoIsNext(state: typeof StateAnnotation.State) {
+  if (state.nextRepresentative.includes("MARKETING")) {
+    return "marketingSupport";
+  } else if (state.nextRepresentative.includes("LEARNING")) {
+    return "LearningSupport";
+  } else if (state.nextRepresentative.includes("RESPOND")) {
+    return "__end__";
+  } else {
+    return "__end__";
+  }
 }
 
 const graph = new StateGraph(StateAnnotation)
   .addNode("frontDeskSupport", frontDeskSupport)
   .addNode("marketingSupport", marketingSupport)
   .addNode("LearningSupport", LearningSupport)
-  .addEdge("__start__", "frontDeskSupport");
+  .addEdge("__start__", "frontDeskSupport")
+  .addConditionalEdges("frontDeskSupport", whoIsNext, {
+    marketingSupport: "marketingSupport",
+    LearningSupport: "LearningSupport",
+    __end__: "__end__",
+  })
+  .addEdge("marketingSupport", "__end__")
+  .addEdge("LearningSupport", "__end__");
+
+const app = graph.compile();
+
+//invoke
+
+async function main() {
+  const stream = await app.stream({
+    messages: [
+      {
+        role: "user",
+        content:
+          "Hi, I would like to know about any discounts available on your web development course.",
+      },
+    ],
+  });
+
+  for await (const value of stream) {
+    console.log("----- New State Update -----");
+    console.log(value);
+    console.log("----------------------------");
+  }
+}
+
+main();
